bf29b8432552c3b64eba1fab7ed7b9f4cee04df4
DSCoderman <dsenour6@gmail.com>
2023-07-11 11:04:25 -0400

    added photo

--- src/Pages/MeInfo/index.js
+++ src/Pages/MeInfo/index.js
@@ -1,13 +1,13 @@
 import { Container } from '@mui/material';
 import React from 'react';
-import AboutMe from '../../Images/AboutMe.jpg'
+import ProfShot from '../../Images/ProfShot.jpg'
 
 
 const MeInfoPage = () => {
     return (
         <>
             <Container>
-            <h1> <img src= {AboutMe} alt="placeholder" width={150}/> </h1>
+            <h1> <img src= {ProfShot} alt="placeholder" width={150}/> </h1>
             <h1> About me</h1>
             <h2> Hi, I am Drew Senour and I am an up and coming fullstack developer! After taking a bootcamp in though Penn in March of 2023, I have been getting my feet wet with multiple languages and technologies to see what I can create!</h2>
             </Container>
